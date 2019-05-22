334bbd1e0dcb19b65c2fe9d2dea31454c06bcdbc
Wiebe van Breukelen <wiebe.van.breukelen@hotmail.nl>
2019-05-22 14:49:09 +0200

    Updated .gitignore

--- practicum-atp/simulator-3.7/main.py
+++ practicum-atp/simulator-3.7/main.py
@@ -1,180 +1,44 @@
-if __name__ == "__main__":
+import time
 
-    import Simulator 
-    import Effector
-    import time
+import Simulator
+import SimulatorControl
+
+if __name__ == "__main__":
     #Create Simulator instance
     simulator = Simulator.Simulator(False)
+    control = SimulatorControl.Factory(simulator)
 
-    #Superclass with Constructor and Update function
-    class BaseClass():       
-        def __init__(self):
-            self.object = None
-
-        def update(self) -> None:
-            self.object.update()
-
-        def printDir(self) -> None:
-            print(f"\nObject: " + str(self.object) + "\n")
-            for e in dir(self.object):
-                if not (e.startswith('__') and e.endswith('__')):
-                    print(e)
-            print("\n==========================\n")
-    
-    class Vessel(BaseClass):    
-        def __init__(self, objectID : str):
-            self.object = simulator._Simulator__plant._vessels[objectID]
-
-        def flowIn(self, amount, colour) -> None:
-            self.object.flowIn(amount, colour)
-
-        def heat(self, state: bool):
-            self.object.heat()
-
-        def setPresence(self, presence: bool):
-            self.object.setPresence(presence)
-        
-        def getFluidAmount(self) -> int:
-            return self.object.getFluidAmount()
-        
-        def getColour(self) -> float:
-            return self.object.getColour()
-
-        def getTemperature(self) -> float:
-            return self.object.getTemperature()
-
-        def getPresence(self) -> bool:
-            return self.object.getPresence()
-        
-        def flow(self) -> None:
-            return self.object.flow()
-            
-    class effector(BaseClass):
-        def __init__(self, objectID : str):
-            self.object = simulator._Simulator__plant._effectors[objectID]
-
-        #Sets the Effector state to on
-        def switchOn(self) -> None:
-            self.object.switchOn()
-        
-        #Sets the Effector state to off
-        def switchOff(self) -> None:
-            self.object.switchOff()
-
-        #Returns the Effector state
-        def isOn(self) -> bool:
-            return self.object.isOn()
-
-    class LED(effector):
-        #Switches between the on and off state
-        def toggel(self) -> None:
-            self.object.toggel()
-
-        #Returns the current colour of the led
-        def getColour(self) -> bool:
-            return self.object.getColour()
-
-    class LCD(effector):
-        #Returns data of the currently on the lcd
-        def getLines(self) -> str:
-            return self.object.getLines()
-        
-        #Sets a string of data on the LCD
-        def pushString(self, s: str) -> None:
-            self.object.pushString(s)
-
-        #Sets 4 lines of white spaces on the lcd
-        def clear(self) -> None:
-            self.object.clear()
-
-        #Sets a singel char on the lcd
-        def put(self, s: str) -> None:
-            self.object.put(s)
-
-    class Sensor(BaseClass):
-        def __init__(self, objectID : str):
-            self.object = simulator._Simulator__plant._sensors[objectID]
-
-        #Returns the sensor data
-        def readValue(self) -> float:
-            return self.object.readValue()
-
-        #Returns the senor data with unit of measurment 
-        def measure(self) -> str:
-            return self.object.measure()
-
-    class PresenceSensor(Sensor):
-        #Returns if the cup is presenced
-        def readValue(self) -> bool:
-            return self.object.readValue()
-    
-    class Keypad(Sensor):
-        #Sets a singel char in the keypad (keypress simulation)
-        def push(self, c: chr) -> None:
-            self.object.push(c)
-
-        #Returns the first char of the Keypad
-        def pop(self) -> str:
-            return self.object.pop()
-
-        #Pushes a string to the keypad
-        def pushString(self, s: str) -> None:
-            for c in s:
-                self.object.push(c)
-        
-        #Pops the complete keypad buffer
-        def popAll(self) -> str:
-            s = ""
-            charBuffer = ''
-            while True:
-                charBuffer = self.object.pop()
-                if charBuffer == '\x00':
-                    return s
-                s += charBuffer
-
-        def readBuffer(self) -> str:
-            s = ""
-            charBuffer = ''
-            self.push('|')
-            while True:
-                charBuffer = self.object.pop()
-                if charBuffer == '|':
-                    return s
-                self.object.push(charBuffer)
-                s += charBuffer
-
-               
     #Creation of objects 
     #print(f"Vessel: " + str(vesselObject.getFluidAmount()))
-    VesselA = Vessel('a')
-    VesselB = Vessel('b')
+    VesselA = control.make(SimulatorControl.Vessel, 'a')
+    VesselB = control.make(SimulatorControl.Vessel, 'b')
 
     #effectors 
-    PumpA       = effector('pumpA')
-    PumpB       = effector('pumpB')
-    ValveA      = effector('valveA')
-    ValveB      = effector('valveB')
-    Heater      = effector('heater')
+    PumpA       = control.make(SimulatorControl.Effector, 'pumpA')
+    PumpB       = control.make(SimulatorControl.Effector, 'pumpB')
+    ValveA      = control.make(SimulatorControl.Effector, 'valveA')
+    ValveB      = control.make(SimulatorControl.Effector, 'valveB')
+    Heater      = control.make(SimulatorControl.Effector, 'heater')
 
     #LED's    
-    LedRedA     = LED('redA')
-    LedGreenA   = LED('greenA')
-    LedRedB     = LED('redB')
-    LedGreenB   = LED('greenB')
-    LedGreenM   = LED('greenM')
-    LedYellowM  = LED('yellowM')
+    LedRedA     = control.make(SimulatorControl.LED, 'redA')
+    LedGreenA   = control.make(SimulatorControl.LED, 'greenA')
+    LedRedB     = control.make(SimulatorControl.LED, 'redB')
+    LedGreenB   = control.make(SimulatorControl.LED, 'greenB')
+    LedGreenM   = control.make(SimulatorControl.LED, 'greenM')
+    LedYellowM  = control.make(SimulatorControl.LED, 'yellowM')
 
     #Sensors
-    Colour      = Sensor('colour')
-    Temperature = Sensor('temp')
-    Level       = Sensor('level')
-    Cup         = PresenceSensor('presence')
+    Colour      = control.make(SimulatorControl.Sensor, 'colour')
+    Temperature = control.make(SimulatorControl.Sensor, 'temp')
+    Level       = control.make(SimulatorControl.Sensor, 'level')
+    Cup         = control.make(SimulatorControl.Sensor, 'presence')
 
     #UI
-    LCDDisplay  = LCD('lcd')
+    LCDDisplay  = control.make(SimulatorControl.LCD, 'lcd')
     #There has to be data in the buffer, before you can write to the buffer(put & pushString)
     LCDDisplay.clear()
-    Keypad      = Keypad('keypad')
+    Keypad      = control.make(SimulatorControl.Keypad, 'keypad')
 
     #A Custom Run function
     def run(debug: bool = True) -> None:
@@ -204,15 +68,15 @@ if __name__ == "__main__":
         print(getVesselLevel(VesselA, VesselB))
 
     #Keeps the fluid om the given temp
-    def heaterOnTemp(targetTemprature: float) -> None:
+    def heaterOnTemp(targetTemperature: float) -> None:
         currentTemprature = Temperature.readValue()
-        if currentTemprature < targetTemprature:
+        if currentTemprature < targetTemperature:
             Heater.switchOn()
         else:
             Heater.switchOff()
 
     #Returns the vessels thier levels
-    def getVesselLevel(VesselA: Vessel, VesselB: Vessel):
+    def getVesselLevel(VesselA: SimulatorControl.Vessel, VesselB: SimulatorControl.Vessel):
         return [VesselA.getFluidAmount(), VesselB.getFluidAmount()]
 
     #Keeps the fluid om the given colour
@@ -277,6 +141,3 @@ if __name__ == "__main__":
     print(list(LCDDisplay.getLines()))
 
     run(False)
-
-
-    
