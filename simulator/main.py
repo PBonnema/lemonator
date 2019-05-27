5b672cb23adb41cba60f68ffc3a2131cd7c15a07
Muscio97 <m.hilhorst@hotmail.nl>
2019-05-27 21:13:25 +0200

    Controller will now be overwritten in main.py, with give us the power to change the interface

--- simulator/main.py
+++ simulator/main.py
@@ -1,8 +1,13 @@
 import Simulator
+import SimulatorInterface
+import Controller
 
 if __name__ == "__main__":
+
     simulator = Simulator.Simulator(True)
     
+    simulator._Simulator__controller = Controller.Controller(simulator._Simulator__plant._sensors, simulator._Simulator__plant._effectors, SimulatorInterface.SimulatorInterface)
+
     simulator.run()
 
 
