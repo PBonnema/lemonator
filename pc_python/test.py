0c416ad54a73e7b7ec6e587b6533f4dc4bc9a16b
Wiebe van Breukelen <wiebe.vanbreukelen@student.hu.nl>
2019-06-19 22:35:58 +0200

    Updated example code.

--- pc_python/test.py
+++ pc_python/test.py
@@ -1,11 +1,23 @@
 import lemonator
 import time
 
-print( "Python interface demo running" )
-hw = lemonator.lemonator( 2 )
-led = hw.led_yellow
-while 1:
-   led.set( 1 )
-   time.sleep( 0.5 )
-   led.set( 0 )
-   time.sleep( 0.5 )
+try:
+
+    print("Starting lemonator...")
+    hw = lemonator.lemonator(1)  # COM2
+    led = hw.led_yellow
+except:
+    exit(0)
+
+temperature = hw.temperature
+
+print("Temperature: ")
+print(temperature.read_mc())
+while True:
+    try:
+        led.set(1)
+        time.sleep(0.5)
+        led.set(0)
+        time.sleep(0.5)
+    except:
+        exit(1)
