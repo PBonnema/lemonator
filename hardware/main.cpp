d56ca1cf06314b1e5afe40bb2e127f2ee75fd84d
Muscio97 <m.hilhorst@hotmail.nl>
2019-06-20 23:28:47 +0200

    Wrote the c++ version of the python controller. No interface support right now

---
+++ hardware/main.cpp
@@ -0,0 +1,16 @@
+#include "CustomController.hpp"
+//#include "PrettyProgressIcon.hpp"
+#include <stdio.h>
+#include <iostream>
+
+int main(void){
+
+    //CustomController controller;
+    //controller.update();
+    //PrettyProgressIcon icon;
+
+    std::cout << "Hello world" << std::endl;
+    
+
+    return 0;
+}
